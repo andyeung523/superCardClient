57336f26ba9d1235459bad1a7c54137a4d003355
Teemo <hohinchan123@gmail.com>
2020-10-08 21:58:20 +0800

    thisistest

---
+++ lib/view/insurance.dart
@@ -0,0 +1,15 @@
+class insurance extends StatefulWidget {
+  insurance({Key key}) : super(key: key);
+
+  @override
+  _insuranceState createState() => _insuranceState();
+}
+
+class _insuranceState extends State<insurance> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
