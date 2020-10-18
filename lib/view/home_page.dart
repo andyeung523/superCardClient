640bba5a90f36ba08f432740727e6533d5c95826
Teemo <hohinchan123@gmail.com>
2020-10-18 22:14:46 +0800

    home_page

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:super_card_client/constants.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'dart:ui' as ui;
+//import 'package:flutter;
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -10,8 +14,200 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('HomePage'),
-    );
+    return Scaffold(
+        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      SizedBox(
+        height: 20,
+      ),
+      Align(
+        alignment: Alignment.centerRight,
+        child: Icon(
+          MdiIcons.faceAgent,
+          color: Constants.kPrimaryColor,
+          size: 30.0,
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+        //color: Colors.grey,
+        child: Text(
+          'Hi, Cyber Youngsters',
+          style: TextStyle(fontSize: 30, color: Colors.black),
+          textAlign: TextAlign.left,
+        ),
+      ),
+      SizedBox(height: 10),
+      Center(
+        child: Container(
+          //margin: EdgeInsets.only(top: 50.0, left: 120.0), //容器外填充
+          constraints:
+              BoxConstraints.tightFor(width: 370.0, height: 226), //卡片大小
+          decoration: BoxDecoration(
+              //背景装饰
+              gradient: LinearGradient(
+                //背景径向渐变
+                colors: [Colors.green, Colors.green[300]],
+                //center: Alignment.centerLeft,
+                //radius: .98
+              ),
+              boxShadow: [
+                //卡片阴影
+                BoxShadow(
+                    color: Colors.black54,
+                    offset: Offset(2.0, 2.0),
+                    blurRadius: 4.0)
+              ]),
+        ),
+      ),
+      SizedBox(height: 15),
+      Container(
+        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        // color: Colors.grey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Account Balance',
+              style: TextStyle(fontSize: 20, color: Colors.black),
+              textAlign: TextAlign.left,
+            ),
+            Text(
+              'HKD 11,500',
+              style: TextStyle(fontSize: 30, color: Colors.black),
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Health Dollar',
+              style: TextStyle(fontSize: 20, color: Colors.black),
+              textAlign: TextAlign.left,
+            ),
+            Row(
+              children: [
+                Icon(Icons.favorite),
+                Text(
+                  '500',
+                  style: TextStyle(fontSize: 30, color: Colors.black),
+                  textAlign: TextAlign.left,
+                ),
+                RawMaterialButton(
+                  onPressed: () {},
+                  elevation: 2.0,
+                  fillColor: Constants.kPrimaryColor,
+                  child: Icon(
+                    MdiIcons.currencyUsdCircleOutline,
+                    color: Colors.white,
+                    size: 35.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+      SizedBox(
+        height: 15,
+      ),
+      Center(
+        child: Container(
+          //color: Colors.amber,
+          //alignment: Alignment.center,
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  //color: Colors.blueGrey,
+                  child: Column(
+                    children: [
+                      RawMaterialButton(
+                        onPressed: () {},
+                        elevation: 2.0,
+                        fillColor: Constants.kPrimaryColor,
+                        child: Icon(
+                          MdiIcons.plus,
+                          color: Colors.white,
+                          size: 35.0,
+                        ),
+                        padding: EdgeInsets.all(15.0),
+                        shape: CircleBorder(),
+                        splashColor: Colors.black,
+                      ),
+                      SizedBox(height: 5),
+                      Text("Add Money"),
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    RawMaterialButton(
+                      onPressed: () {},
+                      elevation: 2.0,
+                      fillColor: Constants.kPrimaryColor,
+                      child: Icon(
+                        MdiIcons.swapHorizontal,
+                        color: Colors.white,
+                        size: 35.0,
+                      ),
+                      padding: EdgeInsets.all(15.0),
+                      shape: CircleBorder(),
+                      splashColor: Colors.black,
+                    ),
+                    SizedBox(height: 5),
+                    Text("Transfer")
+                  ],
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      RawMaterialButton(
+                        onPressed: () {},
+                        elevation: 2.0,
+                        fillColor: Constants.kPrimaryColor,
+                        child: Icon(
+                          MdiIcons.currencyUsdCircleOutline,
+                          color: Colors.white,
+                          size: 35.0,
+                        ),
+                        padding: EdgeInsets.all(15.0),
+                        shape: CircleBorder(),
+                        splashColor: Colors.black,
+                      ),
+                      SizedBox(height: 5),
+                      Text("Loan")
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      RawMaterialButton(
+                        onPressed: () {},
+                        elevation: 2.0,
+                        fillColor: Constants.kPrimaryColor,
+                        child: Icon(
+                          Icons.history,
+                          color: Colors.white,
+                          size: 35.0,
+                        ),
+                        padding: EdgeInsets.all(15.0),
+                        shape: CircleBorder(),
+                        splashColor: Colors.black,
+                        //highlightColor: Colors.green[200],
+                      ),
+                      SizedBox(height: 5),
+                      Text("Transaction")
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      )
+    ]));
   }
 }
